7ff6544c1607b83e876d1e3d4b1097bc9b803ef0
Imrane Mejouate <155024532+Imrane-Mejouate@users.noreply.github.com>
2025-03-21 12:36:41 +0000

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,12 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/svg+xml" href="/images/YouChef-Icon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouChef | Recipes Sharing Platform</title>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
-<body class='scrollbar'>
+<body class="scrollbar">
     @inertia
 </body>
 </html>
