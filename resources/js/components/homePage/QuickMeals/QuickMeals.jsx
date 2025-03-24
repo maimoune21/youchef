f7f703e2fc012b96d83fa6c9678b4f2d7148a981
Imrane_Yt <imrane.uchiha.10@gmail.com>
2025-03-24 11:51:01 +0000

    connect quick meals with meals page

--- resources/js/components/homePage/QuickMeals/QuickMeals.jsx
+++ resources/js/components/homePage/QuickMeals/QuickMeals.jsx
@@ -7,7 +7,7 @@ const QuickMeals = ({ meals, categories }) => {
   return (
     <div className='w-full flex flex-col max-tn:p-0 max-sm:p-0 pb-5 max-md:p-3 bg-soft gap-12'>
       <QuickMeal meals={meals} />
-      {categories.slice(0,2).map((categorie, i)=><QuickMeal key={i} categorie={categorie} meals={meals}/>)}
+      {categories.slice(0,3).map((categorie, i)=><QuickMeal key={i} categorie={categorie} meals={meals}/>)}
     </div>
   );
 }
